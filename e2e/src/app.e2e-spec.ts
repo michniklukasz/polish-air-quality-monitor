4ddcd1d5e4580b820429ce6136eb4da09e89870d
Lukasz Michnik <l.michnik08@gmail.com>
2020-03-26 13:10:07 +0100

    try 1 fix displaying index.html

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('appOne app is running!');
+    expect(page.getTitleText()).toEqual('simple-air-quality-monitor app is running!');
   });
 
   afterEach(async () => {
